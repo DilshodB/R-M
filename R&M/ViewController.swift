55b5d0f3fd008bb96bd47306e95d991536f077c1
DilshodB <nnepke99@gmai.com>
2023-02-08 13:02:05 +0500

    Initial Commit

---
+++ R&M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R&M
+//
+//  Created by 1 1 on 08/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
